d84fd27920eba176d5e289fc69f81cf631c617de
StephenJakku <StephenJakku@users.noreply.github.com>
2022-03-27 06:00:06 +0000

    deploy: 991d9d49e36dcc72835dc0e2ed467d961595f736

--- _next/static/zSh8S-8O5XNm2bGrEroMm/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-d39d3bbd6d6e6208.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
