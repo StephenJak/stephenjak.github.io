b0f8befdbd20caa99c96c061dac4fb2bddbe8237
Stephen Jakku <uttamjakku96@gmail.com>
2022-04-13 00:36:21 -0700

    Test component

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,15 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import Test from "../components/Test";
 
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline text-red-700">Hello world!</h1>
+    <div>
+      <h1 className="text-3xl font-bold underline text-red-700">
+        Hello world!
+      </h1>
+      <Test />
+    </div>
   );
 }
